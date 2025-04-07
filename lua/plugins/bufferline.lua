096cc797ac6788d915f930a182f58cd7fbc5e76d
joevtap <joelvtorres2024@gmail.com>
2025-04-06 22:37:03 -0300

    feat: change theme to catppuccin

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,11 +1,13 @@
 return {
   "akinsho/bufferline.nvim",
+  after = "catppuccin",
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     local bufferline = require("bufferline")
 
     bufferline.setup({
+      highlights = require("catppuccin.groups.integrations.bufferline").get(),
       options = {
         separator_style = "slant",
         numbers = "buffer_id",
