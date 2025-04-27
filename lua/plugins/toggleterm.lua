a8bbf44767454ddc50fcccddaf4e765c7fbdf7dd
joevtap <joelvtorres2024@gmail.com>
2025-04-27 10:36:02 -0300

    feat: remove toggleterm keymap

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("toggleterm").setup({})
 
-    vim.keymap.set("n", "<leader><Esc>", ":ToggleTerm direction=horizontal size=10<CR>", { silent = true })
-    vim.keymap.set("v", "<leader>t", ":ToggleTermSendVisualSelection<CR>", { silent = true })
+    -- vim.keymap.set("n", "<leader><Esc>", ":ToggleTerm direction=horizontal size=10<CR>", { silent = true })
+    -- vim.keymap.set("v", "<leader>t", ":ToggleTermSendVisualSelection<CR>", { silent = true })
   end,
 }
